ef97eefd3b0458ba4db0f059adf8551cbe26939f
Joel Waymack <joel.waymack@microsoft.com>
2022-12-21 11:07:30 -0600

    Update to handle linux line endings and alpine scripting.

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,13 @@
 FROM node:alpine AS build
 WORKDIR /app
 COPY . .
-RUN npm ci && npm run build --prod
+RUN npm ci && npm run build
 
 FROM nginx:alpine
 COPY --from=build /app/dist /usr/share/nginx/html
-COPY --from=build /app/src/startup.sh /app/src/startup.sh
-RUN chmod +x /app/src/startup.sh
+COPY --from=build /app/startup.sh /app/startup.sh
+RUN chmod +x /app/startup.sh
+RUN sed -i 's/\r//' /app/startup.sh
 COPY --from=build /app/nginx.conf /etc/nginx/conf.d/default.conf
 EXPOSE 80
-ENTRYPOINT ["sh", "/app/src/startup.sh"]
+ENTRYPOINT ["sh", "/app/startup.sh"]
--- startup.sh
+++ startup.sh
@@ -2,17 +2,14 @@
 # Retrieve all app config values.
 prefix=APP_ENV_VAR
 script=$(echo "<script id=\""$prefix"\">")
-vars=$(env | awk -F "=" '{print $1}' | grep "$prefix.*")
-
-# Remove the old value (if there) and then write the (new) value.
-while read n; do
+env | awk -F "=" '{print $1}' | grep "$prefix.*" | while read n ; do
     start=$(echo "window."$n" = '")
     val=$(printenv $n)
     line=$(echo $start$val"';")
-    sed -i "\|$start|d" index.html
+    sed -i "\|$start|d" /usr/share/nginx/html/index.html
     awk -v line="$line" -v script="$script" '$0~script { print; print line; next}1' /usr/share/nginx/html/index.html > /usr/share/nginx/html/temp.html
     cp /usr/share/nginx/html/temp.html /usr/share/nginx/html/index.html
-done <<< "$vars"
+done
 
 # Remove the temp file.
 rm /usr/share/nginx/html/temp.html
